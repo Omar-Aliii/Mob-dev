b5b918e8a5259a4696a57f7c167bb5f18c30a196
OmarCringe <omarali_egyptboy2003@yahoo.com>
2023-03-03 00:47:14 +0200

    First

---
+++ tester1/lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+          ),
+          onPressed: selectHandler,
+          child: Text(answerText),
+        ));
+  }
+}
---
+++ tester1/lib/quiz.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final Function changeQuestion;
+  final int questionIndex;
+  Quiz(
+      {required this.questions,
+      required this.changeQuestion,
+      required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Question(questions[questionIndex]['questionText'] as String),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(changeQuestion(), answer);
+        }).toList(),
+      ],
+    );
+  }
+}
